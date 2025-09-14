492cb1c7ca23be694ae35c1c5e53b0b3f293e12c
𝐃 𝐄 𝐋 𝐋 <you@example.com>
2025-09-14 19:23:43 +0530

    I have add some codes

--- routes/Regrouter.js
+++ routes/Regrouter.js
@@ -6,6 +6,9 @@ var NameValidator=require('../validators/Namevalidator')
 
 
 router.post('/newuser',Validator.RegUserValidation,RegController.userRegistration);
-router.post('/singledata',RegController.userRegistration);
+router.post('/singledata',NameValidator.NameUserValidation,RegController.userRegistration);
+router.post('/singledatapswd',NameValidator.PasswordUserValidation,RegController.userRegistration);
+router.post('/singledataphone',NameValidator.PhoneUserValidation,RegController.userRegistration);
+router.post('/singledataemail',NameValidator.EmailUserValidation,RegController.userRegistration);
 
 module.exports = router;
--- validators/Namevalidator.js
+++ validators/Namevalidator.js
@@ -21,4 +21,68 @@ if(error.error){
 }
 };
 
-module.exports= {NameUserValidation};
+
+const PasswordValidation = Joi.object({
+    password: Joi.string().min(6).max(10).required()
+})
+
+const PasswordUserValidation = async (req,res,next) => {
+    const passwordValidator = {
+        password:req.body.name
+    }
+
+const error = PasswordValidation.validate(passwordValidator)
+console.log(error);
+
+if(error.error){
+    var result=error.datails;
+    console.log(result);
+    res.status(404).json({msg:"Un Authorized User",error:result});
+}else{
+    next();
+}
+};
+
+
+const PhoneValidation = Joi.object({
+    phone:Joi.string().length(10).required()
+})
+
+const PhoneUserValidation = async (req,res,next) => {
+    const phoneValidator = {
+        phone:req.body.name
+    }
+
+const error = PhoneValidation.validate(phoneValidator)
+console.log(error);
+
+if(error.error){
+    var result=error.datails;
+    console.log(result);
+    res.status(404).json({msg:"Un Authorized User",error:result});
+}else{
+    next();
+}
+};
+
+const EmailValidation = Joi.object({
+   email:Joi.string().required()
+})
+
+const EmailUserValidation = async (req,res,next) => {
+    const emailValidator = {
+        email:req.body.name
+    }
+
+const error = EmailValidation.validate(emailValidator)
+console.log(error);
+
+if(error.error){
+    var result=error.datails;
+    console.log(result);
+    res.status(404).json({msg:"Un Authorized User",error:result});
+}else{
+    next();
+}
+};
+module.exports= {NameUserValidation,PasswordUserValidation,PhoneUserValidation,EmailUserValidation};
